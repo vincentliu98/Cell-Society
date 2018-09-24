e225607530e86402a5a77daa6c39bb3aeb9a6007
vincentliu98 <jie.liu0731@gmail.com>
2018-09-24 01:47:05 -0400

    added all parameters to each model panels

--- src/visualization/model_panels/SpreadingFirePanel.java
+++ src/visualization/model_panels/SpreadingFirePanel.java
@@ -1,5 +1,9 @@
 package visualization.model_panels;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -7,13 +11,37 @@ import java.util.Map;
  */
 
 public class SpreadingFirePanel extends ModelPanel {
+    public static final double DEFAULT_PROBCATCH = 0.7;
+    private Slider probCatchBar = new Slider(0, 1, DEFAULT_PROBCATCH);
+    private double probCatch;
+    private boolean changeProbCatch;
+
+    public static final Label probCatchCaption = new Label("probCatch:");
+    private Label probCatchValue = new Label(
+            Double.toString(probCatchBar.getValue()));
+
+    public SpreadingFirePanel() {
+        super();
+        probCatchBar.setShowTickMarks(true);
+        probCatchBar.setShowTickLabels(true);
+        probCatchBar.setOnMouseReleased(e -> {
+            changeProbCatch = true;
+            probCatch = probCatchBar.getValue();
+            probCatchValue.setText(String.format("%.2f", probCatch));
+        });
+        getChildren().addAll(probCatchCaption, probCatchValue, probCatchBar);
+        probCatch = DEFAULT_PROBCATCH;
+    }
+
     @Override
     public Map<String, String> getParams() {
-        return null;
+        var ret = new HashMap<String, String>();
+        ret.put("probCatch", Double.toString(probCatch));
+        return ret;
     }
 
     @Override
     public boolean paramsChanged() {
-        return false;
+        return changeProbCatch;
     }
 }
--- src/visualization/model_panels/WaTorPanel.java
+++ src/visualization/model_panels/WaTorPanel.java
@@ -1,5 +1,9 @@
 package visualization.model_panels;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -7,13 +11,84 @@ import java.util.Map;
  */
 
 public class WaTorPanel extends ModelPanel {
+    public static final int DEFAULT_FISHBREEDPERIOD = 4;
+    public static final int DEFAULT_SHARKBREEDPERIOD = 5;
+    public static final int DEFAULT_SHARKSTARVEPERIOD = 5;
+
+    private Slider fbBar = new Slider(0, 10, DEFAULT_FISHBREEDPERIOD);
+    private Slider sbBar = new Slider(0, 10, DEFAULT_SHARKBREEDPERIOD);
+    private Slider ssBar = new Slider(0, 10, DEFAULT_SHARKSTARVEPERIOD);
+
+    private int fb;
+    private int sb;
+    private int ss;
+
+    private boolean changeFB;
+    private boolean changeSB;
+    private boolean changeSS;
+
+    public static final Label fbCaption = new Label("Fish Breed Period:");
+    public static final Label sbCaption = new Label("Shark Breed Period:");
+    public static final Label ssCaption = new Label("Shark Starve Period:");
+
+    private Label fbValue = new Label(
+            Integer.toString((int) fbBar.getValue()));
+    private Label sbValue = new Label(
+            Integer.toString((int) sbBar.getValue()));
+    private Label ssValue = new Label(
+            Integer.toString((int) ssBar.getValue()));
+
+    public WaTorPanel() {
+        super();
+        getStyleClass().add("modelPanel");
+
+        fb = DEFAULT_FISHBREEDPERIOD;
+        sb = DEFAULT_SHARKBREEDPERIOD;
+        ss = DEFAULT_SHARKSTARVEPERIOD;
+
+        fbBar.setShowTickMarks(true);
+        fbBar.setShowTickLabels(true);
+        sbBar.setShowTickMarks(true);
+        sbBar.setShowTickLabels(true);
+        ssBar.setShowTickMarks(true);
+        ssBar.setShowTickLabels(true);
+
+        fbBar.setOnMouseReleased(e -> {
+            changeFB = true;
+            fb = (int) fbBar.getValue();
+            fbValue.setText(Integer.toString(fb));
+        });
+
+        sbBar.setOnMouseReleased(e -> {
+            changeSB = true;
+            sb = (int) sbBar.getValue();
+            sbValue.setText(Integer.toString(sb));
+        });
+
+        ssBar.setOnMouseReleased(e -> {
+            changeSS = true;
+            ss = (int) ssBar.getValue();
+            ssValue.setText(Integer.toString(ss));
+        });
+
+        getChildren().addAll(fbCaption, fbValue, fbBar);
+        getChildren().addAll(sbCaption, sbValue, sbBar);
+        getChildren().addAll(ssCaption, ssValue, ssBar);
+    }
+
+
+
     @Override
     public Map<String, String> getParams() {
-        return null;
+        var ret = new HashMap<String, String>();
+        ret.put("fishBreedPeriod", Integer.toString(fb));
+        ret.put("sharkBreedPeriod", Integer.toString(sb));
+        ret.put("sharkStarvePeriod", Integer.toString(ss));
+        return ret;
     }
 
     @Override
     public boolean paramsChanged() {
-        return false;
+        return changeFB || changeSB || changeSS;
     }
 }
